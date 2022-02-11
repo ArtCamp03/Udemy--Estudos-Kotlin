363b2c30b3a04ea439a0786d45ea5cf7f85d330a
Artur R Campos <arturrc10@outlook.com>
2022-02-11 17:55:46 -0300

    Aulas Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,38 @@
+fun main() {
+    println("Ola Mundo !!!")
+
+    // comentario
+
+    /*
+    Documentaçao inteira ctrl + alt + l
+     */
+
+    // variaveis
+    var nome: String = "Joao"
+    // 0ou
+    var nome2 = "Maria"
+
+    println("Double: MAX: ${Double.MAX_VALUE} - MIN: ${Double.MIN_VALUE}")
+
+    // Numeros unsigned 0 - 255 , numeros sem sinal
+    val abc1: UInt = 0u
+    val abc2: UShort = 10u
+    val abc3: UByte = 20u
+    val abc4: ULong = 30u
+}
+
+
+// tipo
+
+/*
+Type  Bit
+Double  64
+Float   32
+Long    64
+Int     32
+Short   16
+Byte    8
+Boolean ?
+String  ?
+Char    ?
+ */
---
+++ src/Variaveis.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // variaveis mutaveis e imutaveis
+
+    // variavel mutavel
+    var nome = "Lucas"
+
+    // variavel imutavel
+    val name = "Joao"
+    val sobrenome = "Stark"
+
+    println("Ola " + name + " " + sobrenome)
+
+    // jeito do kotlin
+    println("Ola $nome $sobrenome")
+}
