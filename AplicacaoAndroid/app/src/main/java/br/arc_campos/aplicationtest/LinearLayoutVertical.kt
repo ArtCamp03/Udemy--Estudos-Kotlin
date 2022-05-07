3cf64257802fd13608520281512ca5def01fc29b
Artur R Campos <arturrc10@outlook.com>
2022-05-07 13:41:12 -0300

    Linear Layout

---
+++ AplicacaoAndroid/app/src/main/java/br/arc_campos/aplicationtest/LinearLayoutVertical.kt
@@ -0,0 +1,11 @@
+package br.arc_campos.aplicationtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutVertical : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
