264594f3a600d14de63cea9b4ee618eb1c459039
Artur R Campos <arturrc10@outlook.com>
2022-05-08 17:32:20 -0300

    viewBinding

--- GastoViagens/app/src/main/java/br/arc_campos/gastoviagens/MainActivity.kt
+++ GastoViagens/app/src/main/java/br/arc_campos/gastoviagens/MainActivity.kt
@@ -2,10 +2,24 @@ package br.arc_campos.gastoviagens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.arc_campos.gastoviagens.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // View binding
+    // ActivityMainBinding -> classe responsavel por mapear os elementos
+    // inflate -> expande o layout
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+    private fun calculate(){
+    }
+
 }
