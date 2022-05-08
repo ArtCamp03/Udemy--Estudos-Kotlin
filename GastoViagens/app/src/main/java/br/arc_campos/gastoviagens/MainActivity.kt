d77feff313d3575ab882d1af68b3c8148e9453bb
Artur R Campos <arturrc10@outlook.com>
2022-05-08 19:16:55 -0300

    click event

--- GastoViagens/app/src/main/java/br/arc_campos/gastoviagens/MainActivity.kt
+++ GastoViagens/app/src/main/java/br/arc_campos/gastoviagens/MainActivity.kt
@@ -2,9 +2,11 @@ package br.arc_campos.gastoviagens
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import br.arc_campos.gastoviagens.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     // View binding
     // ActivityMainBinding -> classe responsavel por mapear os elementos
@@ -17,9 +19,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // maneira de captura botao calcula
+        // metodo a ser usado neste projeto
+        binding.btnCalc.setOnClickListener(this)
+
+        // 3º maneira
+        // classe Anonima
+        /*
+            binding.btnCalc.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    TODO("Not yet implemented")
+                }
+            })
+
+         */
+
+        // Lambda
+        /*
+            binding.btnCalc.setOnClickListener {
+                TODO("Not yet implemented")
+                /// implementaçao
+            }
+         */
+
+
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.btn_calc){
+            calculate()
+        }
+
     }
 
     private fun calculate(){
+        // tost notification
+        Toast.makeText(this, "Clicado com sucesso!!", Toast.LENGTH_SHORT).show()
     }
 
 }
