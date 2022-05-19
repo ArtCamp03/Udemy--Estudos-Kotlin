5a28c06870ee63bfe11b1db41ed57dae01f6cd1b
Artur R Campos <arturrc10@outlook.com>
2022-05-19 19:47:56 -0300

    implementing view binding

--- MotivationApp/app/src/main/java/br/arc_campos/motivationapp/MainActivity.kt
+++ MotivationApp/app/src/main/java/br/arc_campos/motivationapp/MainActivity.kt
@@ -2,10 +2,29 @@ package br.arc_campos.motivationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import br.arc_campos.motivationapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // esconder barra de nmavegaçao
+        supportActionBar?.hide()
+
+        // eventos
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_new_phrase){
+            var s = ""
+        }
     }
 }
