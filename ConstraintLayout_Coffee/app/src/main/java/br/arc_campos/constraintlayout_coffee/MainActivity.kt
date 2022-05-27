3fc5ba5759f1c1181c0375a50837b93307c4d8b1
Artur R Campos <arturrc10@outlook.com>
2022-05-27 18:00:40 -0300

    coffe aplication

---
+++ ConstraintLayout_Coffee/app/src/main/java/br/arc_campos/constraintlayout_coffee/MainActivity.kt
@@ -0,0 +1,14 @@
+package br.arc_campos.constraintlayout_coffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+    }
+}
