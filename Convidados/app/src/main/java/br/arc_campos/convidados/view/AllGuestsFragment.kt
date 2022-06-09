a22aa14617e2835aa32a1433949f827c13b8bd77
Artur R Campos <arturrc10@outlook.com>
2022-06-09 17:37:34 -0300

    listagem de convidados

--- Convidados/app/src/main/java/br/arc_campos/convidados/view/AllGuestsFragment.kt
+++ Convidados/app/src/main/java/br/arc_campos/convidados/view/AllGuestsFragment.kt
@@ -9,35 +9,40 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import br.arc_campos.convidados.databinding.FragmentHomeBinding
 import br.arc_campos.convidados.viewModel.AllGuestsViewModel
+import kotlinx.android.synthetic.main.nav_header_main.*
 
 class AllGuestsFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var viewModel: AllGuestsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        viewModel.getAll()
+
+        observe()
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observe(){
+        viewModel.guests.observe(viewLifecycleOwner){
+            val s = ""
+            //Lista de convidados
+        }
     }
 }
