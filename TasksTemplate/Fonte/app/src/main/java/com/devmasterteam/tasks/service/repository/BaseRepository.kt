19ee051d417c4ec8c1df4b54695ad66cea1c5a9c
Artur R Campos <arturrc10@outlook.com>
2022-08-08 18:52:12 -0300

    internet connection validation

--- TasksTemplate/Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
+++ TasksTemplate/Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -1,6 +1,9 @@
 package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
@@ -11,23 +14,50 @@ import retrofit2.Response
 
 open class BaseRepository(val context: Context) {
 
-    private fun failResponse(str: String): String{
+    private fun failResponse(str: String): String {
         return Gson().fromJson(str, String::class.java)
     }
 
-    fun <T> executeCall(call: Call<T>, listener: APIListener<T>){
+    fun <T> executeCall(call: Call<T>, listener: APIListener<T>) {
         call.enqueue(object : Callback<Boolean> {
             override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                if(response.code() == TaskConstants.HTTP.SUCCESS){
+                if (response.code() == TaskConstants.HTTP.SUCCESS) {
                     response.body()?.let { listener.onSuccess(it) }
-                }else{
+                } else {
                     listener.onFailure(failResponse(response.errorBody()!!.string()))
                 }
             }
+
             override fun onFailure(call: Call<Boolean>, t: Throwable) {
                 listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
             }
 
         })
+    }
+
+    fun isConnectionAvailable(): Boolean {
+        var result = false
+
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+       if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+           val activityNet = cm.activeNetwork ?: return false
+           val networkCapabilities = cm.getNetworkCapabilities(activityNet) ?: return false
+           result = when {
+               networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+               networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+               else -> false
+           }
+       }else{
+           if(cm.activeNetworkInfo 1= null) {
+               result = when (cm.activeNetworkInfo!!.type) {
+                   ConnectivityManager.TYPE_WIFI -> true
+                   ConnectivityManager.TYPE_MOBILE -> true
+                   ConnectivityManager.TYPE_ETHERNET -> true
+                   else -> false
+               }
+           }
+       }
+
+        return result
     }
 }
--- TasksTemplate/Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ TasksTemplate/Fonte/app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -1,6 +1,7 @@
 package com.devmasterteam.tasks.service.repository
 
 import android.content.Context
+import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.repository.local.TaskDatabase
@@ -40,6 +41,11 @@ class PriorityRepository(context: Context): BaseRepository(context) {
     }
 
     fun list(listener: APIListener<List<PriorityModel>>){
+        if(!isConnectionAvailable()){
+            listener.onFailure(context.getString(R.string.ERROR_INTERNET_CONNECTION))
+            return
+        }
+
         val call = remote.list()
 
     }
