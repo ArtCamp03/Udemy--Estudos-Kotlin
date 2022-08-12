b3c2679fffd70fcabf3bfc780499456cb2f92106
Artur R Campos <arturrc10@outlook.com>
2022-08-12 12:41:28 -0300

    timerpicker e eventos

--- Components/app/src/main/java/br/arc_camp_tcc/components/TimeActivity.kt
+++ Components/app/src/main/java/br/arc_camp_tcc/components/TimeActivity.kt
@@ -1,16 +1,19 @@
 package br.arc_camp_tcc.components
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
 import br.arc_camp_tcc.components.databinding.ActivityTimeBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
-class TimeActivity : AppCompatActivity(), View.OnClickListener , DatePickerDialog.OnDateSetListener{
+class TimeActivity : AppCompatActivity(), View.OnClickListener , DatePickerDialog.OnDateSetListener,
+TimePickerDialog.OnTimeSetListener{
 
     private lateinit var binding: ActivityTimeBinding
 
@@ -22,7 +25,12 @@ class TimeActivity : AppCompatActivity(), View.OnClickListener , DatePickerDialo
         binding = ActivityTimeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Datepicker
         binding.buttonDate.setOnClickListener(this)
+
+        // Timepicker
+        binding.buttonTime.setOnClickListener(this)
+        
 
         supportActionBar?.hide()
     }
@@ -37,7 +45,10 @@ class TimeActivity : AppCompatActivity(), View.OnClickListener , DatePickerDialo
             val year = calendar.get(Calendar.YEAR)
 
             DatePickerDialog(this, this, year, month, day).show()
+        }else if(v.id == R.id.button_time){
+            TimePickerDialog(this, this, 1, 1, false).show()
         }
+
     }
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
@@ -46,9 +57,12 @@ class TimeActivity : AppCompatActivity(), View.OnClickListener , DatePickerDialo
         binding.buttonDate.text = mSimpleDate.format(date)
     }
 
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        toast("$hourOfDay: $minute")
+    }
+
     private fun toast(str: String){
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show()
     }
-
 
 }
